45086ca17178f6662134b32144534dece89362c1
Suther1and <aiur272@gmail.com>
2024-03-19 14:06:20 +0300

    Initial Commit

---
+++ weatherApp.v2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  weatherApp.v2
+//
+//  Created by Pavel Barto on 19.03.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
